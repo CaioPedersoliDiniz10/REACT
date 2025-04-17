1757cd18ae67d2850268e2f8b88a574ff9ed3384
Caio Pedersoli Diniz <131314728+CaioPedersoliDiniz10@users.noreply.github.com>
2025-04-17 14:31:14 -0300

    crud

---
+++ crud/frontend/src/components/template/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Main.css'
+import Header from './Header'
+
+export default props => (
+    <div>
+        <Header />
+        <main className="content">
+            Conteúdo
+        </main>
+    </div>
+)
---
+++ crud/frontend/src/main/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Logo from '../components/template/Logo'
+import Nav from '../components/template/Nav'
+import Footer from '../components/template/Footer'
+import Main from '../components/template/Main'
+
+export default function App() {
+  return (
+    <div>
+      <Logo />
+      <Nav />
+      <Main />
+      <Footer />
+     
+    </div>
+  )
+}
