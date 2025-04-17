0943dc170e43b6a85cb496a2aad681e94f8c01ef
Caio Pedersoli Diniz <131314728+CaioPedersoliDiniz10@users.noreply.github.com>
2025-04-17 15:54:36 -0300

    continuando

--- crud/frontend/src/main/App.jsx
+++ crud/frontend/src/main/App.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import Logo from '../components/template/Logo'
-import Nav from '../components/template/Nav'
-import Footer from '../components/template/Footer'
-import Main from '../components/template/Main'
+import React from 'react';
+import './App.css'
+import Logo from '../components/template/Logo';
+import Nav from '../components/template/Nav';
+import Footer from '../components/template/Footer';
+import Main from '../components/template/Main';
 
 export default function App() {
   return (
-    <div>
+    <div className="app">  {/* Aplicando a classe app aqui */}
       <Logo />
       <Nav />
       <Main />
       <Footer />
-     
     </div>
-  )
+  );
 }
