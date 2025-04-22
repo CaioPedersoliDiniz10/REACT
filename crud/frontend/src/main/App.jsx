795e9cc9c6fb24dd79a9d341b8e9a2eb61a6306c
Caio Pedersoli Diniz <131314728+CaioPedersoliDiniz10@users.noreply.github.com>
2025-04-22 17:15:25 -0300

    .

--- crud/frontend/src/main/App.jsx
+++ crud/frontend/src/main/App.jsx
@@ -1,19 +1,20 @@
- import React from 'react';
- import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
-import Logo from '../components/template/Logo';
-import Nav from '../components/template/Nav';
-import Footer from '../components/template/Footer';
-import Home from '../components/home/Home';
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 
-export default function App() {
-  return (
-    <div className="app">  {/* Aplicando a classe app aqui */}
-      <Logo />
-      <Nav />
-      <Home/>
-      <Footer />
-    </div>
-  );
-}
+import Logo from '../components/template/Logo'
+import Nav from '../components/template/Nav'
+import Routes from './Routes'
+import Footer from '../components/template/Footer'
+
+export default props =>
+    <BrowserRouter>
+        <div className="app">
+            <Logo />
+            <Nav />
+            <Routes />
+            <Footer />
+        </div>
+    </BrowserRouter>
