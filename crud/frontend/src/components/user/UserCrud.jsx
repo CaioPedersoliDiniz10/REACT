34c557a2a8ad9a14034e6015c90e87c654d41748
Caio Pedersoli Diniz <131314728+CaioPedersoliDiniz10@users.noreply.github.com>
2025-04-23 18:42:05 -0300

    .

--- crud/frontend/src/components/user/UserCrud.jsx
+++ crud/frontend/src/components/user/UserCrud.jsx
@@ -1,12 +1,43 @@
 import React, { Component } from 'react'
 import Main from '../template/Main'
+import axios from 'axios'
 
 const headerProps ={
     icon: 'users',
     title: 'Usuarios',
     subtitle: "Cadastro de usuarios: Incluir, Listar, Alterar e Excluir"
 }
+
+const baseUrl = 'http://localhost:3001/users'
+const inicialState = {
+    user: { name: '' , email: ''},
+    list: []
+}
 export default class UserCrud extends Component{
+    state ={ ...inicialState}
+
+    clear(){
+        this.setState({user: inicialState.user})
+    }
+    
+    save(){
+        const user = this.state.user
+        const method = user.id ? 'put' :  'post'
+        const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
+        axios[method](url,user)
+        .then(resp =>{
+            const list = this.getUpadteList(resp.data)
+            this.setState({user: inicialState.user, list})
+        })
+    }
+
+    getUpadteList(user){
+        const list = this.state.list.filter(u=> u.id !== user.id)
+        list.unshift(user)
+        return list
+    }
+
+    
     render(){
         return(
             <Main {...headerProps}>
